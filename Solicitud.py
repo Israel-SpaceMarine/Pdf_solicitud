38372b8a35b3e33bc2a00959d35873f3f9041534
SeñordecapituloIsrael <Noreply@cecapmex.com>
2023-01-20 16:01:39 -0600

    cambios datos

--- Solicitud.py
+++ Solicitud.py
@@ -14,75 +14,67 @@ from openpyxl import load_workbook
 app = Flask(__name__)
 CORS(app)
 
+
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
 @app.route('/solicitud',  methods=['POST'])
-@check_token
+#@check_token
 
+# funcion donde se recibe el json, se procesa y se remueven los xlsx para no tener basura en el cache
 def get_data():
     data = request.data
     data = json.loads(data)
-    for f in glob.iglob('.xlsx', recursive=True):
+    for f in glob.iglob('*.xlsx', recursive=True):
         os.remove(f)
-    return pdf(data)
+    return solicitud(data)
 
+# funcion donde se abre la plantilla del excel donde se depositan los datos 
+def solicitud(data):
+    print(data)
 
-def pdf(data):
+    prueba = data["datos"]["Fecha"]
+    print(prueba)
 
-  
-    Fecha =	data["Fecha"]
-    Tipo_de_movimiento = data["TipoDeMovimiento"]
-    Nombre=	data["Nombre"]
-    Apellido_paterno =	data["ApellidoPaterno"]
-    Apellido_materno =	data["ApellidoMaterno"]
-    Nombre_de_usuario =	data["NombreUsuario"]
-    Correo = data["Correo"]
-    Curp = data["CUPR"]
-    Rfc = data["RFC"]
-    Telefono =	data["Telefono"]
-    Extensión =	data["Extensión"]
-    Celular = data["Celular"]
-    Tipo_de_usuario = data["AccesoApp"]
-    Nota = data["Nota"] 
-    print(Nota)
+    Tipo_de_usuario = data["datos"]["AccesoApp"]
+    
+
+    # se lee la plantilla del excel para su llenado de informacion
     workbook = load_workbook(filename="./templates/template_sol_usuarios.xlsx")
     workbook.sheetnames
-
     sheet = workbook.active
+    # llenado del excel
+    x = data["datos"]["Fecha"]
+    x = x.split("T")
+    y =x[0]
+    w = y.split("-")
+    Fecha = w[2]+"/"+w[1]+"/"+w[0] 
     sheet["C9"] = Fecha
-    sheet["C10"] = Tipo_de_movimiento
-    sheet["C11"] = Nombre
-    sheet["C12"] = Apellido_paterno
-    sheet["C13"] = Apellido_materno
-    sheet["C14"] = Nombre_de_usuario
-    sheet["C15"] = Correo
-    sheet["C16"] = Curp
-    sheet["C17"] = Rfc
-    sheet["C18"] = Telefono
-    sheet["C19"] = Extensión
-    sheet["C20"] = Celular
-    sheet["C21"] = Tipo_de_usuario 
-    
-
+    sheet["C10"] = data["datos"]["TipoDeMovimiento"]
+    sheet["C11"] = data["datos"]["Nombre"]
+    sheet["C12"] = data["datos"]["ApellidoPaterno"]
+    sheet["C13"] = data["datos"]["ApellidoMaterno"]
+    sheet["C14"] = data["datos"]["NombreUsuario"]
+    sheet["C15"] = data["datos"]["Correo"]
+    sheet["C16"] = data["datos"]["CUPR"]
+    sheet["C17"] = data["datos"]["RFC"]
+    sheet["C18"] = data["datos"]["Telefono"]
+    sheet["C19"] = data["datos"]["Extensión"]
+    sheet["C20"] = data["datos"]["Celular"]
+    sheet["C21"] = ""
+    sheet["C22"] = data["datos"]["AccesoApp"]
+    #sheet["A28"] = data["datos"]["Nota"] 
+    # nombre del documento
     filename = Tipo_de_usuario + ".xlsx"
     file_stream = BytesIO()
+    # se guarda el documetno
     workbook.save(Tipo_de_usuario + ".xlsx")
     
-    
-    file_stream.seek(0)
 
+    file_stream.seek(0)
     
-    
-    # excel = client.Dispatch("Excel.Application",pythoncom.CoInitialize())
-    # #app.Interactive = False
-    # #app.Visible = False
-    # #pdfop2 = app.open(filename, "rb")
-    # sheets = excel.Workbooks.Open('C:\\Users\\hp\\OneDrive\\Escritorio\\PDF_solicitud\\'+filename)
-    # work_sheets = sheets.Worksheets[0]
-    # pedf_T = work_sheets.ExportAsFixedFormat(0, 'C:\\Users\\hp\\OneDrive\\Escritorio\\PDF_solicitud\\'+filename)
-    # sheets.close()
-    return send_file(filename , as_attachment=True, )
+    # se envia el documento para su descarga
+    return send_file("./" + filename , as_attachment=True, )
    
 
 
